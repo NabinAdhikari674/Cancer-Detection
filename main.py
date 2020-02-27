391b8df9c1bf526dc279e99ce7e46e0548c84feb
Nabin Adhikari <36256500+NabinAdhikari674@users.noreply.github.com>
2020-02-27 19:57:33 +0530

    Restructured in Project

--- main.py
+++
@@ -1,7 +0,0 @@
-# Main
-#Working Dir : C:\Users\nabin\Nabin\101Projects\Cancer Detection
-print("Cancer Detection Research and Study for Dissertation")
-
-# Extracting Information from DICOM Files
-import custom_utilities
-custom_utilities.ExtractDICOM()
---
+++ sources/main.py
@@ -0,0 +1,5 @@
+# Main
+#Working Dir : C:\Users\user\User\101Projects\Cancer Detection
+print("Cancer Detection Research and Study for Dissertation")
+
+# Coming Soon
